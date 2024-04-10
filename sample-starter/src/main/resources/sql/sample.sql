248b2163c0dcec9437d11aaeef8e8979d991a19e
fish <576471489@qq.com>
2024-04-10 11:27:04 +0800

    1

--- sample-starter/src/main/resources/sql/sample.sql
+++ sample-starter/src/main/resources/sql/sample.sql
@@ -25,3 +25,14 @@ CREATE TABLE `orders`
 
 
 将订单数据放入数仓 es/clickHouse进行计算 得到排行榜
+
+
+秒杀无锁设计:
+可以用lua脚本来执行,redis单线程执行lua脚本不会被其他指令打断,所以具有原子性
+商品记录库存
+用户秒杀的时候 使用lua脚本  , 使用setnx来保证幂等
+
+大致lua流程:
+1.首先获取商品库存,如果无商品,则返回无库存
+2.进行购买,库存减去购买数
+3.用户购买后用setnx 记录 redisKey：product:userId  1 ,用于保障幂等
